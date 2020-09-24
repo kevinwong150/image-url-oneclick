cdff23c3b2c4f3fed00633fcc955c3386a7632c4
kevinwong150 <kevinwyl@yahoo.com.hk>
2020-09-24 23:48:40 +0800

    add options page and core function

--- src/js/background.js
+++ src/js/background.js
@@ -1,23 +1,75 @@
+// init options status
+let isOptionOpen = false;
+let optionsTabId = null;
+
 //click extension icon will save image url in all opening tabs to storage
-chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log(tab);
+chrome.browserAction.onClicked.addListener(function(from_tab) {
   chrome.tabs.getAllInWindow(null, function(tabs){
     let imageURLs = [];
+
+    // find image urls from all tabs
     for (var i = 0; i < tabs.length; i++) {
       let tabURL = tabs[i].url;
       if(isImageURL(tabURL)) {
-        console.log("tab index:", tabs[i].index, "with url", tabURL);
         imageURLs.push(tabURL);
       }
     }
 
-    let timestampKey = Date.now().toString();
-    let setValue = imageURLs.join("|");
-    chrome.storage.sync.set({[timestampKey]: setValue}, function() {
-      console.log('Value is set to ' + setValue);
-    });
+    if(imageURLs.length > 0) {
+      // open options page
+      chrome.runtime.openOptionsPage(function() {
+        let timestampKey = Date.now();
+        let setValue = imageURLs.join("|");
+        
+        // put newly saved urls
+        chrome.storage.sync.set({[timestampKey.toString()]: setValue}, function() {
+          if (isOptionOpen) {
+          // tell options page update UI on-the-fly
+          chrome.runtime.sendMessage({pendingOptionAction: "updateList"}, function(response) {
+            if(response) {
+              console.log("optionStatus: ", response["optionStatus"]);
+            }
+            else {
+              console.log("options page may not be opening");
+            }
+          });
+          }
+        });
+      });
+    }
   });
 });
+
+// message listener 
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  switch (request.optionStatus) {
+    case null:
+    case undefined:
+      sendResponse({backgroundStatus: "null/undefined"});
+      break;
+    case "opened":
+      isOptionOpen = true;
+      optionsTabId = request.optionsTabId ? request.optionsTabId : optionsTabId;
+      sendResponse({backgroundStatus: "updated: options page opened"});
+      break;
+    case "closed":
+      isOptionOpen = false;
+      sendResponse({backgroundStatus: "updated: options page closed"});
+      break;
+    default:
+      sendResponse({backgroundStatus: "noop"});
+      break;
+  }
+});
+
+// reset options status on any tab closed 
+chrome.tabs.onRemoved.addListener((closedTabId) => {
+  if(closedTabId == optionsTabId) {
+    isOptionOpen = false;
+    optionsTabId = null;
+  }
+});
+
 
 function isImageURL(url) {
   // finding suffix:
--- src/js/options.js
+++ src/js/options.js
@@ -1,31 +1,79 @@
-// Saves options to chrome.storage
-function save_options() {
-  var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
-  chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
+function clear_all_records() {
+  chrome.storage.sync.clear(() => {
+    document.getElementById('saved-content').innerHTML = "this is empty!";
   });
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
-  }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
+function restore_records() {
+  chrome.storage.sync.get(null, function(records) {
+    // terminate if no storage
+    if (Object.keys(records).length === 0) 
+      return;
+    
+    let savedContent = document.getElementById('saved-content');
+    let dd = document.createElement("dd"); 
+    savedContent.innerHTML = "";
+    dd.innerHTML = "";
+
+    for (timestamp in records) {
+      let listItem = `
+        <dt>
+          <b>${(new Date(parseInt(timestamp))).toLocaleString()}</b>
+        </dt>
+        <dd>
+          ${records[timestamp]}
+        </dd>
+      `;
+
+      dd.innerHTML = dd.innerHTML + listItem;
+    }
+
+    savedContent.appendChild(dd);
   });
-}
-document.addEventListener('load', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
+};
+
+// message listener 
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    switch (request.pendingOptionAction) {
+      case null:
+      case undefined:
+        console.log("optionStatus:", "null/undefined");
+        sendResponse({optionStatus: "null/undefined recieved"});
+        break;
+      case "updateList":
+        restore_records();
+        console.log("optionStatus:", "updated");
+        sendResponse({optionStatus: "updated"});
+        break;
+      default:
+        sendResponse({optionStatus: "noop"});
+        break;
+    }
+});
+
+// tell background page options page is up
+document.addEventListener('DOMContentLoaded', () => {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var optionsTab = tabs[0];
+    if (currTab) {
+      chrome.runtime.sendMessage({
+        optionStatus: "opened",
+        optionsTabId: optionsTab.id
+      }, function(response) {
+        if(response) {
+          console.log("backgroundStatus: ", response["backgroundStatus"]);
+        }
+        else {
+          console.log("background script may not received");
+        }
+      });
+    }
+  });
+});
+
+// build options page on DOMContentLoaded
+document.addEventListener('DOMContentLoaded', restore_records);
+
+// clear all storage
+document.getElementById('clear-all').addEventListener('click', clear_all_records);
