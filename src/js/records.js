b46853a020f0463537a2734fa93d12cd366ca6f3
kevinwong150 <kevinwyl@yahoo.com.hk>
2020-09-30 15:05:39 +0800

    fix remove all record to not remove settings

--- src/js/records.js
+++ src/js/records.js
@@ -11,97 +11,106 @@ chrome.storage.sync.get([
 
 // keep settings up-to-date
 chrome.storage.onChanged.addListener((changes, areaName) => {
-  console.log((areaName === "sync" && "settings-removeAllConfirmation?" in changes));
   if(areaName === "sync" && "settings-removeAllConfirmation?" in changes) {
     hasConfirm = changes["settings-removeAllConfirmation?"]["newValue"];
   }
 });
 
 function clear_all_records() {
-  if(hasConfirm) {
-    if(confirm("This will remove all listed records, are you sure?")) {
-      chrome.storage.sync.clear(() => {
+  chrome.storage.sync.get(null, function(records) {
+    // terminate if no storage
+    if (Object.keys(records).filter((record) => /^settings-/.test(record)).length === 0)
+      return;
+
+    if(hasConfirm) {
+      if(confirm("This will remove all listed records, are you sure?")) {
+        for (timestamp in records) {
+          if((new Date(parseInt(timestamp)).getTime())) chrome.storage.sync.remove(timestamp);
+        }
         document.getElementById('records-body').innerHTML = "There is no record yet";
-      });
+      }
     }
-  }
-  else {
-    chrome.storage.sync.clear(() => {
+    else {
+      for (timestamp in records) {
+        if((new Date(parseInt(timestamp)).getTime())) chrome.storage.sync.remove(timestamp);
+      }
       document.getElementById('records-body').innerHTML = "There is no record yet";
-    });
-  }
+    }
+  });
 }
 
 function restore_records() {
   chrome.storage.sync.get(null, function(records) {
     // terminate if no storage
-    if (Object.keys(records).length === 0) 
+    if (Object.keys(records).filter(record => !(/^settings-.+/.test(record))).length === 0) {
       return;
-    
-    let recordsContent = document.getElementById('records-body');
-    let recordList = document.createElement("ul"); 
-    recordsContent.innerHTML = "";
-    recordList.classList = [ "record-list" ];
-    recordList.innerHTML = "";
-
-    for (timestamp in records) {
-      // ignore settings if invalid timestamp 
-      if(!(new Date(parseInt(timestamp)).getTime())) continue;
-
-      let record = records[timestamp];
-
-      // create list item by js createElement so we could add eventListener to it easily
-      let listItem = document.createElement("li"); 
-      listItem.classList = [ "record-list-item" ];
-      listItem.setAttribute("data-key", timestamp);
-      listItem.innerHTML = `
-        <div class="record-title">
-          <button class="record-title-item button mod-remove" title="Delete record"></button>
-          <span class="record-title-item mod-date">${(new Date(parseInt(timestamp))).toLocaleString()}</span>
-          <span class="record-title-item mod-count">Count: ${record["count"]}</span>
-          <button class="record-title-item button mod-copy" title="Copy URLs"></button>
-        </div>
-        <div class="record-content">
-          <span class="record-content-item mod-urls">${record["urls"]}</span>
-        </div>
-      `;
-
-      recordList.appendChild(listItem);
-      
-      // put the remove record function
-      let removeButton = listItem.querySelector("button.record-title-item.mod-remove");
-      if(removeButton) {
-        removeButton.addEventListener('click', () => {
-          chrome.storage.sync.remove(listItem.getAttribute("data-key"), () => {
-            listItem.remove();
-          });
-        });
-      }
-
-      // put the copy to clipboard function
-      let copyButton = listItem.querySelector("button.record-title-item.mod-copy");
-      if(copyButton) {
-        copyButton.addEventListener('click', () => {
-          navigator.clipboard.writeText(record["urls"]).then(function() {
-            copyButton.classList.add("mod-success");
-            copyButton.innerText = "Copied!";
-            setTimeout(() => {
-              copyButton.classList.remove("mod-success");
-              copyButton.innerText = "";
-            }, 2000);
-          }, function() {
-            copyButton.classList.add("mod-fail");
-            copyButton.innerText = "Copy action failed! Please try again.";
-            setTimeout(() => {
-              copyButton.classList.remove("mod-fail");
-              copyButton.innerText = "";
-            }, 2000);
-          });
-        });
-      }
     }
-
-    recordsContent.appendChild(recordList);
+    else {
+      let recordsContent = document.getElementById('records-body');
+      let recordList = document.createElement("ul"); 
+      recordsContent.innerHTML = "";
+      recordList.classList = [ "record-list" ];
+      recordList.innerHTML = "";
+  
+      for (timestamp in records) {
+        // ignore settings if invalid timestamp 
+        if(!(new Date(parseInt(timestamp)).getTime())) continue;
+  
+        let record = records[timestamp];
+  
+        // create list item by js createElement so we could add eventListener to it easily
+        let listItem = document.createElement("li"); 
+        listItem.classList = [ "record-list-item" ];
+        listItem.setAttribute("data-key", timestamp);
+        listItem.innerHTML = `
+          <div class="record-title">
+            <button class="record-title-item button mod-remove" title="Delete record"></button>
+            <span class="record-title-item mod-date">${(new Date(parseInt(timestamp))).toLocaleString()}</span>
+            <span class="record-title-item mod-count">Count: ${record["count"]}</span>
+            <button class="record-title-item button mod-copy" title="Copy URLs"></button>
+          </div>
+          <div class="record-content">
+            <span class="record-content-item mod-urls">${record["urls"]}</span>
+          </div>
+        `;
+  
+        recordList.appendChild(listItem);
+        
+        // put the remove record function
+        let removeButton = listItem.querySelector("button.record-title-item.mod-remove");
+        if(removeButton) {
+          removeButton.addEventListener('click', () => {
+            chrome.storage.sync.remove(listItem.getAttribute("data-key"), () => {
+              listItem.remove();
+            });
+          });
+        }
+  
+        // put the copy to clipboard function
+        let copyButton = listItem.querySelector("button.record-title-item.mod-copy");
+        if(copyButton) {
+          copyButton.addEventListener('click', () => {
+            navigator.clipboard.writeText(record["urls"]).then(function() {
+              copyButton.classList.add("mod-success");
+              copyButton.innerText = "Copied!";
+              setTimeout(() => {
+                copyButton.classList.remove("mod-success");
+                copyButton.innerText = "";
+              }, 2000);
+            }, function() {
+              copyButton.classList.add("mod-fail");
+              copyButton.innerText = "Copy action failed! Please try again.";
+              setTimeout(() => {
+                copyButton.classList.remove("mod-fail");
+                copyButton.innerText = "";
+              }, 2000);
+            });
+          });
+        }
+      }
+  
+      recordsContent.appendChild(recordList);
+    }
   });
 };
 
