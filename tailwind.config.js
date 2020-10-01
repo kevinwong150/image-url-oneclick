3697c76bbb4c8b5cd69ffde2ede9b24abf1ebec4
kevinwong150 <kevinwyl@yahoo.com.hk>
2020-10-02 04:14:37 +0800

    migrating to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+// const
+const headerHeight = "74px";
+const screenWidthThreshold = "1280px";
+
 module.exports = {
   future: {
     // removeDeprecatedGapUtilities: true,
@@ -7,7 +11,21 @@ module.exports = {
     './src/**/*.js',
   ],
   theme: {
-    extend: {},
+    colors: {
+      "light-light": "var(--light-light)",
+      light: "var(--light)",
+      "dark-light": "var(--dark-light)",
+      dark: "var(--dark)",
+      "dark-dark": "var(--dark-dark)",
+      transparent: "var(--transparent)",
+      success: "var(--correct)",
+      error: "var(--incorrect)",
+    },
+    extend: {
+      spacing: {
+        header: headerHeight,
+      },
+    },
   },
   variants: {},
   plugins: [],
